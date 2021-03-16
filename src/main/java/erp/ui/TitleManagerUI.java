b594ad9fa10a31558413970a6129e1d6caa3c17a
yjkim <chitose_@naver.com>
2021-03-16 10:08:15 +0900

    10:08

--- src/main/java/erp/ui/TitleManagerUI.java
+++ src/main/java/erp/ui/TitleManagerUI.java
@@ -1,5 +1,66 @@
 package erp.ui;
 
+import java.awt.event.ActionEvent;
+
+import erp.dto.Title;
+import erp.ui.content.AbstractContentPanel;
+import erp.ui.list.AbstractCustomTablePanel;
+
+@SuppressWarnings("serial")
 public class TitleManagerUI extends AbstractManagerUI<Title> {
+
+	@Override
+	protected void setService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void tableLoadData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected AbstractContentPanel<Title> createContentPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected AbstractCustomTablePanel<Title> createTablePanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void actionPerformedMenuGubun() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void actionPerformedMenuUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void actionPerformedMenuDelete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void actionperformedBtnUpdate(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void actionPerformedBtnAdd(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
